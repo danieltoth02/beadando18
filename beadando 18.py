49d4b8060961ddfddacbb71de3dfccda1a30065c
Tóth Dániel <63225508+danieltoth02@users.noreply.github.com>
2020-05-04 08:38:25 +0200

    Update beadando 18.py

--- beadando 18.py
+++ beadando 18.py
@@ -1,8 +1,8 @@
-lista=[]
+lista=[]                                    #ures lista
 szam=int(input("szam: "))
-for i in (str(szam)):
-    lista.append(szam%10)
-    szam=szam//10
-lista=lista[::-1]
-for j in range (len(lista)):
-    lista[j]=int(lista[j])
+for i in (str(szam)):         
+    lista.append(szam%10)                   #megjelenitjuk a listaban a szam 10-el valo maradekos osztasat (pld. 993; 3 -> lista)
+    szam=szam//10                           #a szamrol le vagjuk az utolso egesz reszt (pld. 993; szam=99)
+lista=lista[::-1]                           #vegul a listat megforditva megkapjuk a szamokat kulon-kulon (pld. "9","9","3")
+for j in range (len(lista)):                #bejarjuk a lista hosszat
+    lista[j]=int(lista[j])                  #egesz szamra alakitunk minden sztringet
