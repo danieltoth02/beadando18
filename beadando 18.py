17496113365cc1c32e277c81950f118be76e7aa8
Tóth Dániel <63225508+danieltoth02@users.noreply.github.com>
2020-05-11 23:24:15 +0200

    Update beadando 18.py

--- beadando 18.py
+++ beadando 18.py
@@ -1,8 +1,22 @@
-lista=[]
-szam=int(input("szam: "))
-for i in (str(szam)):
-    lista.append(szam%10)
-    szam=szam//10                         
-lista=lista[::-1]                         
-for j in range (len(lista)):            
-    lista[j]=int(lista[j])                
+def torespont (szam):
+    lista = []
+    szam2=szam
+    for i in (str(szam)):
+        lista.append(szam%10)
+        szam=szam//10
+    lista=lista[::-1]
+    for j in range (len(lista)):
+        lista[j]=int(lista[j])
+    for k in range (1,len(lista)):
+        osszeg1 = 0
+        osszeg2 = 0
+        for i in range (0,k):
+            osszeg1=osszeg1+lista[i]
+        for j in range (k,len(lista)):
+            osszeg2=osszeg2+lista[j]
+        if osszeg1==osszeg2:
+            szam2=str(szam2)
+            return (szam2[:k]+"|"+szam2[k:])
+    return False
+print (torespont(336))
+#pld. szam=336   ^^^
